d927f7a366d07d482bc45c41a95bc84c5131faf3
Naveen Michaud-Agrawal <naveenm@enthought.com>
2012-04-23 11:30:40 -0400

    Don't scale marker when zooming

--- mapping/primitives/geo_marker.py
+++ mapping/primitives/geo_marker.py
@@ -20,6 +20,8 @@ class GeoMarker(GeoPrimitive):
 
     def _filename_changed(self, new):
         self._marker = Image(new)
+        # Set anchor to middle bottom
+        self.anchor = (self._marker.width()/2, 0)
 
     ###########################################################################
     # Protected 'Component' interface.
@@ -30,6 +32,11 @@ class GeoMarker(GeoPrimitive):
         with gc: 
             x, y = self.position
             anchor_x, anchor_y = self.anchor
+
+            zoom = max([v.zoom for v in self.container.viewports])
+
+            gc.scale_ctm(1./zoom, 1./zoom)
+            gc.translate_ctm(-x*(1-zoom), -y*(1-zoom))
 
             w, h = self._marker.width(), self._marker.height()
             gc.draw_image(self._marker, (x-anchor_x, y-anchor_y, w, h))
