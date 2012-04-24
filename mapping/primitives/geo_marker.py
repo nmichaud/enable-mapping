473b1e4e005e22e2bf40902acedbba8ed5dd31c5
Naveen Michaud-Agrawal <naveenm@enthought.com>
2012-04-24 10:46:35 -0400

    Marker anchor point should be relative

--- mapping/primitives/geo_marker.py
+++ mapping/primitives/geo_marker.py
@@ -11,8 +11,8 @@ class GeoMarker(GeoPrimitive):
 
     bounds = (1, 1)
 
-    # The anchor point of the marker
-    anchor = coordinate_trait
+    # The anchor point of the marker (in relative coordinates)
+    anchor = coordinate_trait((0.5, 0.))
 
     filename = Str
 
@@ -20,8 +20,6 @@ class GeoMarker(GeoPrimitive):
 
     def _filename_changed(self, new):
         self._marker = Image(new)
-        # Set anchor to middle bottom
-        self.anchor = (self._marker.width()/2, 0)
 
     ###########################################################################
     # Protected 'Component' interface.
@@ -33,12 +31,14 @@ class GeoMarker(GeoPrimitive):
             x, y = self.position
             anchor_x, anchor_y = self.anchor
 
+            # FIXME - this is broken when there are multiple
+            # viewports
             zoom = max([v.zoom for v in self.container.viewports])
 
             gc.scale_ctm(1./zoom, 1./zoom)
             gc.translate_ctm(-x*(1-zoom), -y*(1-zoom))
 
             w, h = self._marker.width(), self._marker.height()
-            gc.draw_image(self._marker, (x-anchor_x, y-anchor_y, w, h))
+            gc.draw_image(self._marker, (x-anchor_x*w, y-anchor_y*h, w, h))
         return
 
