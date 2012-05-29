6b036cffed01348df3797f29cf0d67ee46297c2d
Naveen Michaud-Agrawal <naveenm@enthought.com>
2012-05-29 10:00:34 -0400

    Added setup information and license

---
+++ mapping/__init__.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2007-2012 by Enthought, Inc.
+#  All rights reserved.
+""" Tile-map based components for Enable and Chaco
+    Part of the Enable project of the Enthought Tool Suite.
+"""
+__version__ = '1.0.0'
+
+__requires__ = [ 
+    'enable',
+    'chaco',
+]
---
+++ setup.py
@@ -0,0 +1,45 @@
+# All rights reserved.
+from os.path import join
+from setuptools import setup, find_packages
+
+
+info = {}
+execfile(join('mapping', '__init__.py'), info)
+
+
+setup(
+    name = 'mapping',
+    version = info['__version__'],
+    author = 'Enthought, Inc.',
+    author_email = 'info@enthought.com',
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
+    #url = 'https://github.com/enthought/enable-mapping',
+    #download_url = ('http://www.enthought.com/repo/ets/enable-mapping-%s.tar.gz' %
+    #                info['__version__']),
+    classifiers = [c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines() if len(c.strip()) > 0], 
+    description = 'application tools',
+    long_description = open('README.rst').read(),
+    include_package_data = True,
+    package_data = dict(mapping=[
+    ]), 
+    install_requires = info['__requires__'],
+    license = 'BSD',
+    packages = find_packages(),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    zip_safe = True,
+)
